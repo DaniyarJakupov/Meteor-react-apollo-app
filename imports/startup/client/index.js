b6fa94cd2b0140f40a49d48ac4b138f27d81ae49
DaniyarJakupov <ddr199305@gmail.com>
2018-01-03 22:10:01 +0100

    Connect react to apollo

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import App from '../../ui/app';
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: Meteor.absoluteUrl('graphql') }), // connect to graphql server
+  cache: new InMemoryCache(),
+});
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById('app'));
+  render(<ApolloApp />, document.getElementById('app'));
 });
--- imports/ui/app.js
+++ imports/ui/app.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const App = () => (
-  <div>
-    <h2>UI</h2>
-  </div>
-);
+class App extends Component {
+  state = {};
 
-export default App;
+  render() {
+    const { data } = this.props;
+
+    if (data.loading) {
+      return (
+        <div>
+          <h2>Loading</h2>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h2>
+          {data.getUser.username} - {data.getUser.email}
+        </h2>
+      </div>
+    );
+  }
+}
+
+const GET_USER_QUERY = gql`
+  {
+    getUser {
+      username
+      email
+    }
+  }
+`;
+
+export default graphql(GET_USER_QUERY)(App);
