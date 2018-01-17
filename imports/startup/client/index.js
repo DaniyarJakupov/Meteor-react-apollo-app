07788d16b6a7933f242cb6e01f18d40238066c20
DaniyarJakupov <ddr199305@gmail.com>
2018-01-17 21:52:15 +0100

    Rename app.js

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import App from '../../ui/app';
+import App from '../../ui/App';
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: Meteor.absoluteUrl('graphql') }), // connect to graphql server
